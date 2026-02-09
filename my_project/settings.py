d38efdcff193a1f035127cbcee9db25630fbb1ee
ABHISHEKSHARMA <asp23122004@gmail.com>
2026-02-09 22:41:58 +0530

    Update render.yaml with correct

--- my_project/settings.py
+++ my_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-8p_u7#i7vkp34%q1ei*#5vjz+xv^ow8&z)hmrw6nj#sf(y3+a%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -124,7 +124,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_URL = '/static/'
 
-STATICFILES_DIRS=[
-    BASE_DIR,'STATIC'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
